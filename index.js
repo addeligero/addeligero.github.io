2be6ba60e109ce5d88248a56d38977e17df1410a
ADRIAN <adriandeligero.1030@gmail.com>
2024-09-10 13:21:58 +0800

    added preload

--- index.js
+++ index.js
@@ -128,7 +128,28 @@ document.getElementById("nav3").addEventListener("click", () => {
 
 /**For links */
 /**For preload */
-$(window).on("load", () => {
-  $(".loader-wrapper").fadeOut("slow");
-});
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 2.5,
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
 /**For preload */
